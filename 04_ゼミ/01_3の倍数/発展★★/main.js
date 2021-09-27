bbd5327b3ab291f12d55dda0335b83e1ae6e7f86
darse73 <k.yu.713558.2016113@gmail.com>
2021-09-27 18:16:51 +0900

    [カウンター]を学習しました。

---
+++ 04_ゼミ/01_3の倍数/発展★★/main.js
@@ -0,0 +1,9 @@
+const genkiFunction = function(number) {
+    for(let n = 1; n <= number; n++){
+        if(n % 3 === 0 || n.toString().includes(3)){
+            console.log(`${n}!!!!!!!`)
+        }else{
+            console.log(n)
+        }
+    }
+}
