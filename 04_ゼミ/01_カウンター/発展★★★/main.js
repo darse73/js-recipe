ffaa9d8cf4927462fbcc4aefe7e620b5b231996d
darse73 <k.yu.713558.2016113@gmail.com>
2021-09-29 10:12:16 +0900

    [カウンター発展問題]を解きました。

---
+++ 04_ゼミ/01_カウンター/発展★★★/main.js
@@ -0,0 +1,39 @@
+let displayTop = document.getElementById("display-top")
+let displayBottom = document.getElementById("display-bottom")
+
+function dis(value) {
+    if(displayTop.value == 0){ /*===はなぜだめ？*/
+        displayTop.value = "" + value
+    }else{
+        displayTop.value += value
+    }
+}
+
+function calc() {
+    if(displayTop.value.includes("×")){/*valueの値は文字列？*/
+        displayTop.value = displayTop.value.replace("×","*")
+    }
+    if(displayTop.value.includes("÷")){/*valueの値は文字列？*/
+        displayTop.value = displayTop.value.replace("÷","/")
+    }
+    displayBottom.value = new Function("return " + displayTop.value)()
+    if(displayTop.value.includes("*")){/*valueの値は文字列？*/
+        displayTop.value = displayTop.value.replace("*","×")
+    }
+    if(displayTop.value.includes("/")){/*valueの値は文字列？*/
+        displayTop.value = displayTop.value.replace("/","÷")
+    }
+}
+
+function del() {
+    displayTop.value = displayTop.value.slice(0,-1)
+    displayBottom.value = 0
+    if(displayTop.value === ""){
+        displayTop.value = 0
+    }
+}
+
+function ac() {
+    displayTop.value = 0
+    displayBottom.value = 0
+}
