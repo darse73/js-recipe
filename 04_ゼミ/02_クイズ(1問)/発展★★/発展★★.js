1e8688673dca0f2ae5cc43e5e936c35643456488
darse73 <k.yu.713558.2016113@gmail.com>
2021-10-08 00:42:14 +0900

    [クイズ(1問)発展]を学習しました。

---
+++ 04_ゼミ/02_クイズ(1問)/発展★★/発展★★.js
@@ -0,0 +1,68 @@
+const quizText = document.getElementById("quiz-text")
+const quizImage = document.getElementById("quiz-image")
+const feedback = document.getElementById("feedback")
+const button = document.getElementById("button")
+
+
+
+const quiz = {
+    text: "くらげの平均寿命は？",
+    img: "jerryfish.png",
+    choices: [
+        {
+            text: "半年~1年",
+            feedback: "せいかい！ ちなみに不老不死のクラゲもいるみたい"
+        },
+        {
+            text: "約10年",
+            feedback: "一般的な平均寿命としてはちがうよ！"
+        },
+        {
+            text: "約50年",
+            feedback: "一般的な平均寿命としてはちがうよ！"
+        },
+        {
+            text: "約100年",
+            feedback: "一般的な平均寿命としてはちがうよ！"
+        }
+    ]
+}
+
+const reloadQuiz = function() {
+    quizText.textContent = `問題！　${quiz.text}`
+    quizImage.src = quiz.img
+    const button1 = document.createElement("button")
+    button.append(button1)
+    const button2 = document.createElement("button")
+    button.append(button2)
+    const button3 = document.createElement("button")
+    button.append(button3)
+    const button4 = document.createElement("button")
+    button.append(button4)
+    button1.textContent = quiz.choices[0].text
+    button2.textContent = quiz.choices[1].text
+    button3.textContent = quiz.choices[2].text
+    button4.textContent = quiz.choices[3].text
+
+    const choose = function(choiceNumber) {
+        feedback.textContent = quiz.choices[choiceNumber].feedback
+    }
+
+    button1.onclick = function() {
+        choose(0)
+    }
+    
+    button2.onclick = function() {
+        choose(1)
+    }
+    
+    button3.onclick = function() {
+        choose(2)
+    }
+    
+    button4.onclick = function() {
+        choose(3)
+    }
+}
+
+reloadQuiz()
